e00fa3d9544e3eb959d48f0e04c4d2666b0d212f
Abhigyan Singh <abhigyan2311@gmail.com>
2015-03-28 02:21:27 +0530

    Login Page UI Completed

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!Doctype html>
 <html>
 <head>
     <meta charset="utf-8">
@@ -10,92 +11,88 @@
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
 	<title>Lemniscate | Login</title>
-
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/materialize.css">
+    <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/ajax.js"></SCRIPT>
 </head>
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
-<script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
-<script type="text/javascript" src="js/materialize.min.js"></script>
-<script type="text/javascript" src="js/ajax.js"></SCRIPT>
 
 <body class="bg-login">
 	<div class="login-screen">
 		<div class="panel-login blur-content">
-			<h3 class="white-text" align="center">Lemniscate</h3>
+			<h3 class="white-text hcenter" style="padding-top:10px;">Lemniscate</h3>
 
 			<div id="pane-login" class="panel-body active">
-				<div><h2>Login to Dashboard</h2></div>
-				<div class="">
-                  <form class="col s12" action='<?php echo $_SERVER["PHP_SELF"];?>'  method="POST">
-                    <div class="row">
-                      <div class="input-field col s12">
-                        <input id="uname_id" type="text" class="validate white-text">
-                        <label for="uname_id">Username</label>
-                      </div>
-                      <div class="input-field col s12">
-                        <input id="pword_id" type="password" class="validate white-text">
-                        <label for="pword_id">Password</label>
-                      </div>
-                     <div class="remcheck col s12">
-                     	<input type="checkbox" id="save_login" />
-                        <label for="save_login">Remember Me</label>
-                     </div>
-                     <div class="col s12" style="margin-top:50px;" align="center">
-                     	<button class="btn waves-effect waves-light #03a9f4 light-blue" type="submit" name="action">Submit
-                          <i class="mdi-content-send right"></i>
-                        </button>
-                     </div>
-                    </div>
-                  </form>
+				<div><div class="row">
+                  <div class="col s12">
+                    <ul class="tabs">
+                      <li class="tab col s3"><a href="#tab1">Login</a></li>
+                      <li class="tab col s3"><a href="#tab2">New User</a></li>
+                    </ul>
+                  </div>
+                  <div id="tab1" class="col s12">
+                                 <form class="col s12" action='<?php echo $_SERVER["PHP_SELF"];?>'  method="POST">
+                                  <div class="row">
+                                    <div class="input-field col s12" style="margin-top:30px;">
+                                      <input id="uname_id" type="text" class="validate white-text">
+                                      <label for="uname_id">Username</label>
+                                    </div>
+                                    <div class="input-field col s12">
+                                      <input id="pword_id" type="password" class="validate white-text">
+                                      <label for="pword_id">Password</label>
+                                    </div>
+                                   <div class="remcheck col s12">
+                                   	<input type="checkbox" id="save_login" />
+                                      <label for="save_login">Remember Me</label>
+                                   </div>
+                                   <div class="col s12" style="margin:40px 0 10px 0; text-align:center;">
+                                   	<button class="btn waves-effect waves-light #03a9f4 light-blue" type="submit" name="action">Submit
+                                        <i class="mdi-content-send right"></i>
+                                      </button>
+                                   </div>
+                                   <div class="col s12 hcenter">
+                                   	<a class="waves-effect waves-light btn-flat white-text">Forgot Password</a>
+                                   </div></div>
+                                </form>
+                  </div>
+
+                  <div id="tab2" class="col s12">
+                                  <form class="col s12" action='<?php echo $_SERVER["PHP_SELF"];?>'  method="POST">
+                                  <div class="row">
+                                    <div class="input-field col s12" style="margin-top:30px;">
+                                      <input id="regno_id" type="text" class="validate white-text">
+                                      <label for="regno_id">Registration Number</label>
+                                    </div>
+                                   <div class="input-field col s12">
+                                      <input id="email_id" type="email" class="validate white-text">
+                                      <label for="email_id">Email-ID</label>
+                                    </div>
+                                    <div class="input-field col s12">
+                                    <label for="birthdate">Birthdate</label>
+                                    <input id="birthdate" type="text" class="datepicker white-text">
+                                    </div>
+                                    <div class="col s12" style="margin:40px 0 10px 0; text-align:center;">
+                                   	   <button class="btn waves-effect waves-light #03a9f4 light-blue" type="submit" name="action">Register
+                                        <i class="mdi-content-send right"></i>
+                                      </button>
+                                   </div></div>
+                                </form>
+                  </div>
                 </div>
-			</div><!--#login.panel-body-->
-
-			<div id="pane-create-account" class="panel-body">
-				<h2>Create a New Account</h2>
-				<div class="form-group">
-					<div class="inputer">
-						<div class="input-wrapper">
-							<input type="text" class="form-control" placeholder="Enter your full name">
-						</div>
-					</div>
-				</div><!--.form-group-->
-				<div class="form-group">
-					<div class="inputer">
-						<div class="input-wrapper">
-							<input type="email" class="form-control" placeholder="Enter your email address">
-						</div>
-					</div>
-				</div><!--.form-group-->
-				<div class="form-group">
-					<div class="inputer">
-						<div class="input-wrapper">
-							<input type="password" class="form-control" placeholder="Enter your password">
-						</div>
-					</div>
-				</div><!--.form-group-->
-				<div class="form-group">
-					<div class="inputer">
-						<div class="input-wrapper">
-							<input type="password" class="form-control" placeholder="Enter your password again">
-						</div>
-					</div>
-				</div><!--.form-group-->
-				<div class="form-group">
-					<label><input type="checkbox" name="remember" value="1"> I have read and agree to the term of use.</label>
-				</div>
-				<div class="form-buttons clearfix">
-					<button type="submit" class="btn btn-white pull-left show-pane-login">Cancel</button>
-					<button type="submit" class="btn btn-success pull-right">Sign Up</button>
-				</div><!--.form-buttons-->
-			</div><!--#login.panel-body-->
-
-		</div><!--.blur-content-->
-	</div><!--.login-screen-->
+                </div>
+			</div>
+			</div></div>
 
 	<div class="bg-blur dark">
 		<div class="overlay"></div><!--.overlay-->
 	</div><!--.bg-blur-->
 
-
+<script type="text/javascript"> $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 50 // Creates a dropdown of 15 years to control year
+  });</script>
+                
 </body>
 </html>
