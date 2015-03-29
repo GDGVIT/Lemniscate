ab3a627e9b41a9eb4c88b496957c4442e80c019e
Abhigyan Singh <abhigyan2311@gmail.com>
2015-03-29 22:00:42 +0530

    Login Page UI Completed

--- index.php
+++ index.php
@@ -190,38 +190,6 @@ if (is_null($json))
 	
 }
 ?>
-<!DOCTYPE HTML>
-<HTML>
-<HEAD>
-<TITLE>Leminiscate</TITLE>
-<script type="text/javascript" src="js/ajax.js"></SCRIPT>
-</HEAD>
-<BODY>
-
-<!-- Login Form-->
-<FORM action= "<?php echo $_SERVER["PHP_SELF"];?>"  method="POST">
-Username:<INPUT TYPE="text" id="uname_id" name="uname_id" placeholder="Username" autocomplete="off">
-Password:<INPUT TYPE="password" id="pword_id" name="pword_id" placeholder="Password" autocomplete="off"><br>
-<INPUT TYPE="checkbox" id="save_login" name="save_login" checked value="save_login">Keep me logged in<br>
-<INPUT TYPE="submit" id="login" name="login" value="Login"><br>
-</FORM>
-
-<!-- Forget Password -->
-<INPUT TYPE="button" id="forgot_pword" name="forgot_pword" value="Forgot Password?"><br>
-
-New User:
-<br>
-
-<!-- Registration Form -->
-<FORM action= "<?php echo $_SERVER["PHP_SELF"];?>"  method="POST">
-Registration Number:<INPUT TYPE="text" id="regno_id" name="regno_id" placeholder="12MSE0363" autocomplete="off"><br>
-Date of Birth:<INPUT type = "date" value ="1994-10-01" id = 'dob' name='dob'><br>
-Email : <INPUT TYPE="text" id="email_id" name="email_id" placeholder="xyz@vit.ac.in" autocomplete="off"><br>
-<INPUT TYPE="submit" id="register" name="register" value="Register">
-</FORM>
-</BODY>
-</HTML>
-=======
 <!Doctype html>
 <html>
 <head>
