66ee6c2a2887e5d5fb5d019e06a9ee5a89d82398
Abhigyan Singh <abhigyan2311@gmail.com>
2015-04-03 01:30:47 +0530

    Incorrect Credentials Done

--- index.php
+++ index.php
@@ -56,160 +56,8 @@ else if (isset($_COOKIE['cow']) && isset($_COOKIE['calf']))
 	}
 	mysqli_close($mysqli);
  }
-
-//When the user wants to login
-if(isset($_POST["login"]))
-{
-	require("Database/sql_con.php");
-	$uname=$_POST["uname_id"];
-	$pword=$_POST["pword_id"];
-	$stmt = $mysqli->prepare("SELECT * FROM `login` WHERE `uid`=?  AND `password`=?");
-	$stmt->bind_param("ss", $uname, $pword);	
-	$uname_db="";
-	$pword_db="";
-	if($stmt->execute())
-	{
-		if($rs = $stmt->get_result())
-		{
-			$count = mysqli_num_rows($rs);
-			while ($arr = mysqli_fetch_array($rs)) 
-			{
-				$uname_db = $arr["uid"];
-				$pword_db = $arr["password"];	
-				$gen_id = $arr["gen_id"];		
-			}
-			if(($count==1&&$uname!=""&&$pword!=""&&strcmp($uname,$uname_db)==0)&&(strcmp($pword,$pword_db)==0))
-			{
-				if (isset($_POST["save_login"]))
-				{
-						//Cookie to be created for auto login when "Remember me" has been chosen and the saved id and password expires in 60 days
-						setcookie('cow',base64_encode($uname),time()+3600*24*60);
-						setcookie('calf',base64_encode($pword),time()+3600*24*60);
-				}
-				$_SESSION["gen_id"]=base64_encode($gen_id);
-				header("location:home.php");
-			}
-			else
-			{
-				echo 'Invalid Username or Password';
-			}
-		}
-		else
-			echo"Result set not fetched mysqli_error()";
-	}
-	else
-		echo "Query not executed mysqli_error()";
-mysqli_close($mysqli);
-}
-else if(isset($_POST["get_password"]))
-{
-	require("Database/sql_con.php");
-	
-	$email_f = $_POST["email_forgot"];
-	$regno_f = $_POST["regno_forgot"];
-	
-	$gen_id_db ="";
-	$regno_db="";
-	$email_db="";
-	
-	$stmt = $mysqli->prepare("SELECT `gen_id`, `regno` FROM `info_user` WHERE `email`=? AND `regno`=? ");
-	$stmt->bind_param("ss", $email_f,$regno_f);	
-	if($stmt->execute())
-	{
-		if($rs = $stmt->get_result())
-		{
-			$count = mysqli_num_rows($rs);
-			while ($arr = mysqli_fetch_array($rs)) 
-			{
-				$email_db =  $arr["email"];
-				$regno_db = $arr["regno"];
-				$gen_id_db = $arr["gen_id"];		
-			}
-			if(($count==1&&$email_f!=""&&$regno_f!=""&&strcmp($email_f,$email_db)==0)&&(strcmp($regno_f,$regno_db)==0))
-			{
-				$RandomStr = base64_encode(microtime());
-				$ResultStr = substr($RandomStr,0,20);
-				$ResultStr = strtolower($ResultStr);
-				$activated=0;
-				$date=date("Y-m-d");
-				$stmt = $mysqli->prepare("INSERT INTO `forgot_password` ( `gen_id`, `hash`, `date_apply`, `email` ,`activated`) VALUES (?, ?, ?, ?, ?)");
-				$stmt->bind_param("issi", $gen_id_db, $ResultStr,$date,$email_db,$activated);	
-				if($stmt->execute())
-				{
-					date_default_timezone_set('Asia/Calcutta');
-					require 'mail/PHPMailerAutoload.php';
-					//Create a new PHPMailer instance
-					$mail = new PHPMailer();
-					/*
-					if($mail->smtpConnect())
-					{
-					*/
-							$to= $email; 
-							$subject= "Leminiscate | Password Reset" ;
-							$message="Pls check this link localhost/lemniscate/password_reset.php?p=$ResultStr&e=$email_db&d=$date";
-							//Tell PHPMailer to use SMTP
-							$mail->isSMTP();
-
-							//Enable SMTP debugging
-							$mail->SMTPDebug = 0;						
-							$mail->Host = 'smtp.gmail.com';
-
-							//Set the SMTP port number - 465 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-							$mail->Port =  587;
-
-							//Set the encryption system to use - ssl (deprecated) or tls
-							$mail->SMTPSecure = 'tls';
-
-							//Whether to use SMTP authentication
-							$mail->SMTPAuth = true;
-
-							//Username to use for SMTP authentication - use full email address for gmail
-							$mail->Username = "gdgriviera@gmail.com";
-
-							//Password to use for SMTP authentication
-							$mail->Password = "gdgriviera1";
-
-							//Set who the message is to be sent from
-							$mail->setFrom('gdgriviera@gmail.com', 'Leminiscate Verification');
-
-							//Set an alternative reply-to address
-							$mail->addReplyTo('gdgriviera@gmail.com', 'Leminiscate Verification');
-
-							//Set who the message is to be sent to
-							$mail->addAddress($to, 'Student');
-
-							//Set the subject line
-							$mail->Subject = $subject;
-
-							//Replace the plain text body with one created manually
-							$mail->Body = $message;
-
-							//send the message, check for errors
-							if (!$mail->send())
-							{
-								echo "Mailer Error: " . $mail->ErrorInfo;
-							}
-							else 
-							{
-								echo "Message sent! Check your mail for resetting your password";
-							}
-				/*
-					}
-					else
-						echo "Mailer connection problem";
-				*/
-				}
-				else
-				{
-					echo "Error in inserting the data into forget_passwords";	
-				}
-			
-			}
-		}
-	}
-	mysqli_close($mysqli);
-}
 ?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -404,3 +252,160 @@ function showtab3()
                 
 </body>
 </html>
+
+<?php
+
+if(isset($_POST["login"]))
+{
+	require("Database/sql_con.php");
+	$uname=$_POST["uname_id"];
+	$pword=$_POST["pword_id"];
+	$stmt = $mysqli->prepare("SELECT * FROM `login` WHERE `uid`=?  AND `password`=?");
+	$stmt->bind_param("ss", $uname, $pword);	
+	$uname_db="";
+	$pword_db="";
+	if($stmt->execute())
+	{
+		if($rs = $stmt->get_result())
+		{
+			$count = mysqli_num_rows($rs);
+			while ($arr = mysqli_fetch_array($rs)) 
+			{
+				$uname_db = $arr["uid"];
+				$pword_db = $arr["password"];	
+				$gen_id = $arr["gen_id"];		
+			}
+			if(($count==1&&$uname!=""&&$pword!=""&&strcmp($uname,$uname_db)==0)&&(strcmp($pword,$pword_db)==0))
+			{
+				if (isset($_POST["save_login"]))
+				{
+						//Cookie to be created for auto login when "Remember me" has been chosen and the saved id and password expires in 60 days
+						setcookie('cow',base64_encode($uname),time()+3600*24*60);
+						setcookie('calf',base64_encode($pword),time()+3600*24*60);
+				}
+				$_SESSION["gen_id"]=base64_encode($gen_id);
+				header("location:home.php");
+			}
+			else
+			{
+				echo '<script>toast("Incorrect Username/Password", 3000, "#e53935 red darken-1");</script>';
+			}
+		}
+		else
+			echo"Result set not fetched mysqli_error()";
+	}
+	else
+		echo "Query not executed mysqli_error()";
+mysqli_close($mysqli);
+}
+//When the user wants to login
+
+if(isset($_POST["get_password"]))
+{
+	require("Database/sql_con.php");
+	
+	$email_f = $_POST["email_forgot"];
+	$regno_f = $_POST["regno_forgot"];
+	
+	$gen_id_db ="";
+	$regno_db="";
+	$email_db="";
+	
+	$stmt = $mysqli->prepare("SELECT `gen_id`, `regno` FROM `info_user` WHERE `email`=? AND `regno`=? ");
+	$stmt->bind_param("ss", $email_f,$regno_f);	
+	if($stmt->execute())
+	{
+		if($rs = $stmt->get_result())
+		{
+			$count = mysqli_num_rows($rs);
+			while ($arr = mysqli_fetch_array($rs)) 
+			{
+				$email_db =  $arr["email"];
+				$regno_db = $arr["regno"];
+				$gen_id_db = $arr["gen_id"];		
+			}
+			if(($count==1&&$email_f!=""&&$regno_f!=""&&strcmp($email_f,$email_db)==0)&&(strcmp($regno_f,$regno_db)==0))
+			{
+				$RandomStr = base64_encode(microtime());
+				$ResultStr = substr($RandomStr,0,20);
+				$ResultStr = strtolower($ResultStr);
+				$activated=0;
+				$date=date("Y-m-d");
+				$stmt = $mysqli->prepare("INSERT INTO `forgot_password` ( `gen_id`, `hash`, `date_apply`, `email` ,`activated`) VALUES (?, ?, ?, ?, ?)");
+				$stmt->bind_param("issi", $gen_id_db, $ResultStr,$date,$email_db,$activated);	
+				if($stmt->execute())
+				{
+					date_default_timezone_set('Asia/Calcutta');
+					require 'mail/PHPMailerAutoload.php';
+					//Create a new PHPMailer instance
+					$mail = new PHPMailer();
+					/*
+					if($mail->smtpConnect())
+					{
+					*/
+							$to= $email; 
+							$subject= "Leminiscate | Password Reset" ;
+							$message="Pls check this link localhost/lemniscate/password_reset.php?p=$ResultStr&e=$email_db&d=$date";
+							//Tell PHPMailer to use SMTP
+							$mail->isSMTP();
+
+							//Enable SMTP debugging
+							$mail->SMTPDebug = 0;						
+							$mail->Host = 'smtp.gmail.com';
+
+							//Set the SMTP port number - 465 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+							$mail->Port =  587;
+
+							//Set the encryption system to use - ssl (deprecated) or tls
+							$mail->SMTPSecure = 'tls';
+
+							//Whether to use SMTP authentication
+							$mail->SMTPAuth = true;
+
+							//Username to use for SMTP authentication - use full email address for gmail
+							$mail->Username = "gdgriviera@gmail.com";
+
+							//Password to use for SMTP authentication
+							$mail->Password = "gdgriviera1";
+
+							//Set who the message is to be sent from
+							$mail->setFrom('gdgriviera@gmail.com', 'Leminiscate Verification');
+
+							//Set an alternative reply-to address
+							$mail->addReplyTo('gdgriviera@gmail.com', 'Leminiscate Verification');
+
+							//Set who the message is to be sent to
+							$mail->addAddress($to, 'Student');
+
+							//Set the subject line
+							$mail->Subject = $subject;
+
+							//Replace the plain text body with one created manually
+							$mail->Body = $message;
+
+							//send the message, check for errors
+							if (!$mail->send())
+							{
+								echo "Mailer Error: " . $mail->ErrorInfo;
+							}
+							else 
+							{
+								echo "Message sent! Check your mail for resetting your password";
+							}
+				/*
+					}
+					else
+						echo "Mailer connection problem";
+				*/
+				}
+				else
+				{
+					echo "Error in inserting the data into forget_passwords";	
+				}
+			
+			}
+		}
+	}
+	mysqli_close($mysqli);
+}
+?>
