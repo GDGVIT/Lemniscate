0a5c48e9f017287745f6a58b8200e0dad6925798
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-06-16 17:19:06 +0530

    Removed submit button onSelect functionality
    Now, user doesn't require to click on Submit to fetch results. User just
    need to change the option.

--- sec_hand/show_items.php
+++ sec_hand/show_items.php
@@ -1,7 +1,9 @@
 <html>
 <body>
-<form id="show_category" method="GET" action="show_category.php">
-Category<select name="category">
+
+<form id="show_category" method="GET" action="show_items.php">
+Category<select name="category" onchange='this.form.submit()'>
+		<option type="radio" value = "" selected>Select Category</option>
 		<option type="radio" value="all">Show All</option>
 		<option type = "radio"  value="Electrical">Electrical</option><br>
 		<option type = "radio"  value="Household">Household</option><br>
@@ -9,8 +11,50 @@ Category<select name="category">
 		<option type = "radio"  value="Category 4">Category 4</option><br>
 		<option type = "radio"  value="Category 5">Category 5</option><br>
 	</select>
-<input type = "submit" value="Search category">	
+	<noscript><input type="submit" value="Submit"></noscript>
 </form>
+
+	<?php
+		if(isset($_GET['category'])){
+			$conn=mysql_connect("localhost","root","");
+$db=mysql_select_db("db");
+$category=$_GET['category'];
+if($category=="all"){
+	$sql="select * from sell where sold=0";
+$id=1;
+$result=mysql_query($sql,$conn) or die(mysql_error());
+	while ($row=mysql_fetch_array($result)) {
+
+		echo "ID: ".$id."<br>";
+		echo "Name: ".$row['name']."<br>";
+		echo "Category: ".$row['category']."<br>";
+		echo "Description: ".$row['description']."<br>";
+		echo "Cost: ".$row['cost']."<br>";
+		echo "Contact Number: ".$row['contact']."<br>";
+		echo "<hr>";
+		$id++;
+	}
+
+}
+else {
+$sql="select * from sell where sold=0 and category='{$category}'";
+$id=1;
+$result=mysql_query($sql,$conn) or die(mysql_error());
+	while ($row=mysql_fetch_array($result)) {
+
+		echo "ID: ".$id."<br>";
+		echo "Name: ".$row['name']."<br>";
+		echo "Category: ".$row['category']."<br>";
+		echo "Description: ".$row['description']."<br>";
+		echo "Cost: ".$row['cost']."<br>";
+		echo "Contact Number: ".$row['contact']."<br>";
+		echo "<hr>";
+		$id++;
+	}
+}
+
+		}
+	?>
 <script type="text/javascript">
 $('#show_category').form({
     success:function(data){
--- sec_hand/wishlist.php
+++ sec_hand/wishlist.php
@@ -1,16 +1,61 @@
 <html>
 <body>
-<form id="show_category" method="GET" action="show_wishlist.php">
-Category<select name="category">
-		<option type="radio" value="all">Show All</option>
+
+
+<form id="show_category" method="GET" action="wishlist.php">
+Category<select name ="category" onchange='this.form.submit()'>
+		<option type = "radio" value="" selected>Choose Category</option>
+		<option type = "radio" value="all">Show All</option>
 		<option type = "radio"  value="Electrical">Electrical</option><br>
 		<option type = "radio"  value="Household">Household</option><br>
 		<option type = "radio"  value="Category 3">Category 3</option><br>
 		<option type = "radio"  value="Category 4">Category 4</option><br>
 		<option type = "radio"  value="Category 5">Category 5</option><br>
 	</select>
-<input type = "submit" value="Search category">	
+	<noscript><input type="submit" value="Submit"></noscript>
 </form>
+<?php
+ if (isset($_GET['category'])) {
+ 	$conn=mysql_connect("localhost","root","");
+$db=mysql_select_db("db");
+$category=$_GET['category'];
+if($category=="all"){
+	$sql="select * from buy where status=0";
+$id=1;
+$result=mysql_query($sql,$conn) or die(mysql_error());
+	while ($row=mysql_fetch_array($result)) {
+
+		echo "ID: ".$id."<br>";
+		echo "Name: ".$row['name']."<br>";
+		echo "Category: ".$row['category']."<br>";
+		echo "Expected Cost: ".$row['expected_cost']."<br>";
+		echo "Contact Number: ".$row['contact']."<br>";
+		echo "<hr>";
+		$id++;
+	}
+
+}
+else {
+$sql="select * from buy where status=0 and category='{$category}'";
+$id=1;
+$result=mysql_query($sql,$conn) or die(mysql_error());
+	while ($row=mysql_fetch_array($result)) {
+
+		echo "ID: ".$id."<br>";
+		echo "Name: ".$row['name']."<br>";
+		echo "Category: ".$row['category']."<br>";
+		echo "Cost: ".$row['expected_cost']."<br>";
+		echo "Contact Number: ".$row['contact']."<br>";
+		echo "<hr>";
+		$id++;
+	}
+	if($id=1)
+		echo "No items matched your preference";
+}
+
+ }
+?>
+
 <script type="text/javascript">
 $('#show_category').form({
     success:function(data){
